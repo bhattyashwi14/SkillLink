f5d25c769feb4c897a90959e142c980e380c9b26
KhushiMulchandani <mulchandanikhushi829@gmail.com>
2026-01-29 18:24:13 +0530

    Landing page with Dark Mode

---
+++ core/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    USER_TYPES = (
+        ('student', 'Student'),
+        ('tutor', 'Tutor'),
+        ('company', 'Company'),
+    )
+    
+    # user = models.OneToOneField(User, on_register=True, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user_type = models.CharField(max_length=10, choices=USER_TYPES, default='student')
+    bio = models.TextField(blank=True)
+    
+    # Data Science ready fields
+    # Stored as "Python, Django, SQL"
+    skills = models.TextField(help_text="Enter skills separated by commas", blank=True)
+    
+    # Stored for Matplotlib visualization
+    rating = models.FloatField(default=0.0)
+    total_reviews = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.user_type}"
+
+    def get_skills_list(self):
+        return [s.strip() for s in self.skills.split(',')]
